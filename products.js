a19830246de9565da17ae85d915f579912796fa8
Aditya Shendkar <aditya.shendkar03@gmail.com>
2024-03-16 23:32:41 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: " Chicken Burger",
+    price: 19.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/b.png",
+  },
+  {
+    id: 1,
+    name: "Cup-Cake",
+    price: 14.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/cupcake.png",
+  },
+  {
+    id: 2,
+    name: "Chicken Platter",
+    price: 29.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/food-png-2962.png",
+  },
+  {
+    id: 3,
+    name: "Crispy Chicken",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/friedchicken.png",
+  },
+  {
+    id: 4,
+    name: "Green Salad",
+    price: 15.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/greensalad.png",
+  },
+  {
+    id: 5,
+    name: "Ice Cream Sundae",
+    price: 19.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/icecream.png",
+  },
+];
